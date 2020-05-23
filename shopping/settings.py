fc0aec2deb86eb595da0c23422f23420e370910a
Alex Donald <aldon80@gmail.com>
2020-05-23 17:00:07 +1000

    Changes to able to deploy

--- shopping/settings.py
+++ shopping/settings.py
@@ -161,3 +161,7 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
