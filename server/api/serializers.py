ccdd768ff2710ff47170635333bdc11c9a3b2798
Alex Donald <aldon80@gmail.com>
2020-05-31 18:32:00 +1000

    Try to use base64 for image

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework_json_api import serializers
 from server.models import ShoppingItem
+from drf_extra_fields.fields import Base64ImageField
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
     included_serializers = {
         'user': 'accounts.api.serializers.UserSerializer',
     }
 
+    image = Base64ImageField()
+
+    def create(self, validated_data):
+        image = validated_data.pop('image')
+        name = validated_data.pop('name')
+        price = validated_data.pop('price')
+        user = self.request.user
+        return ShoppingItem.objects.create(name=name, image=image, price=price, user=user)
+        
     class Meta:
         model = ShoppingItem
         fields = (
@@ -17,3 +27,4 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
             # Foreign Key
             'user',
         )
+
--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,4 @@
+from django.core.files.base import ContentFile
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework_json_api.views import ModelViewSet
@@ -22,12 +23,25 @@ class ShoppingItemViewSet(ModelViewSet):
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def perform_create(self, serializer):
-        logging.error(serializer.data)
-        item = serializer.save()
-        item.user = self.request.user
-        item.save()
-        return item
+    # def perform_create(self, serializer):
+    #     import base64
+    #     import pdb; pdb.set_trace()
+    #     data = serializer.data['image']
+    #     format, imgstr = data.split(';base64,')
+    #     ext = format.split('/')[-1]
+    #     data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+    #     logging.error(serializer.data)
+    #     item = serializer.save()
+    #     item.user = self.request.user
+    #     item.save()
+    #     return item
+
+    def post(self, request, *args, **kwargs):
+        serializer = ShoppingItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
     class Meta:
         model = ShoppingItem
