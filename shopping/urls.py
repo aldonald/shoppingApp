75233cf280dc8cdc98d3d4baee6c4ae9ee4cf558
Alex Donald <aldon80@gmail.com>
2020-06-02 00:15:56 +1000

    Change url

--- shopping/urls.py
+++ shopping/urls.py
@@ -35,7 +35,6 @@ router = routers.DefaultRouter()
 router.register(r'shoppingitems', ShoppingItemViewSet, 'shoppingitems')
 router.register(r'add_shopping_item', AddShoppingItemViewSet, 'add_shopping_item')
 router.register(r'accounts', UserViewSet, 'accounts_api')
-router.register(r'add_ftoken', AccountTokenView, 'add_firebase_token')
 router.register(r'pi_add_item', AddItemFromPi, 'pi_add_item')
 
 urlpatterns = [
@@ -45,7 +44,8 @@ urlpatterns = [
     path('', include('server.urls')),
     url(r'^api/', include((router.urls))),
     url(r'^api-token-auth/', views.obtain_auth_token),
-    url(r'^api/create_user', CreateUserView.as_view())
+    url(r'^api/create_user', CreateUserView.as_view()),
+    url(r'add_ftoken', AccountTokenView.as_view(), 'add_firebase_token'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
